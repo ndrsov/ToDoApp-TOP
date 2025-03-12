92927e25ccabba9f817a16196af8d1cd2875cd48
Andres Soria <andres.zov@gmail.com>
2025-03-12 14:43:31 -0600

    Create DOM utilities for the creation of UI elements

---
+++ src/utils/dom-utils.js
@@ -0,0 +1,41 @@
+// Create DOM element with correct atributes
+export function createEl(tag, attributes = {}, textContent = '') {
+  const element = document.createElement(tag);
+
+  Object.keys(attributes).forEach((key) => {
+    if (key === 'className') {
+      element.className = attributes[key];
+    } else {
+      element.setAttribute(key, attributes[key]);
+    }
+  });
+
+  if (textContent) {
+    element.textContent = textContent;
+  }
+
+  return element;
+}
+
+// Remove child elements from parent element
+export function clearEl(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+// Modal functions
+export function showModal(modalid) {
+  const modal = document.getElementById(modalid);
+  modal.classList.add('active');
+}
+
+export function hideModal(modalid) {
+  const modal = document.getElementById(modalid);
+  modal.classList.remove('active');
+}
+
+// Generate unieu ID
+export function generteId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
